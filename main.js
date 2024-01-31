628bd26cb7baa75612824cdea3010824e212f0bd
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-01-31 13:50:41 +0100

    Minimum hozzáadása

--- main.js
+++ main.js
@@ -2,7 +2,21 @@ let szamok=[]
 
 
 document.getElementById("gombplusz").addEventListener("click",()=>{
-    szamok.push(document.getElementById("input").value)
+    szamok.push(parseInt(document.getElementById("input").value))
     document.getElementById("szamok").innerText=szamok
 })
 
+document.getElementById("min").addEventListener("click",()=>{
+    document.getElementById("szamok").innerText=Min(szamok)
+    console.log(Min(szamok))
+})
+
+function Min(szamoko) {
+    let min = szamoko[0];
+    for(let i = 0; i<szamoko.length;i++){
+        if(szamoko[i]<min){
+            min = szamoko[i]
+        }
+    }
+    return min;
+}
