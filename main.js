2f2b5848be1af54839e8acf5735ac0e8d4ea8f09
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-01-31 13:31:54 +0100

    Számok tárolása listában

---
+++ main.js
@@ -0,0 +1,7 @@
+let szamok=[]
+
+
+document.getElementById("gombplusz").addEventListener("click",()=>{
+    szamok.join(document.getElementById("input").value)
+    console.log(document.getElementById("input").value)
+})
