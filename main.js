29bbb48432762ecdd9fe82cedc47978d5d4101a4
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2024-01-31 13:37:34 +0100

    A begépelt számok veszővel való kiírása

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let szamok=[]
 
 
 document.getElementById("gombplusz").addEventListener("click",()=>{
-    szamok.join(document.getElementById("input").value)
-    console.log(document.getElementById("input").value)
+    szamok.push(document.getElementById("input").value)
+    document.getElementById("szamok").innerText=szamok
 })
+
